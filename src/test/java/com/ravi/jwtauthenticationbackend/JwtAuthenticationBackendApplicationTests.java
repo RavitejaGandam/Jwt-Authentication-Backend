cbc72e4e8dbb1274f9da45bf230c7f09729149cc
raviteja gandam <gandamraviteja@gmail.com>
2024-03-26 16:36:30 +0530

    Initial Commit

---
+++ src/test/java/com/ravi/jwtauthenticationbackend/JwtAuthenticationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.jwtauthenticationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
