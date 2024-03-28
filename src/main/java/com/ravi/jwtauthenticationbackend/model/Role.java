a093d493fea20287ae08bf07fe75191a86aaa9b5
raviteja gandam <gandamraviteja@gmail.com>
2024-03-28 11:05:08 +0530

    Partial Dev

---
+++ src/main/java/com/ravi/jwtauthenticationbackend/model/Role.java
@@ -0,0 +1,6 @@
+package com.ravi.jwtauthenticationbackend.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/ravi/jwtauthenticationbackend/service/UserDetailsImpl.java
@@ -0,0 +1,20 @@
+package com.ravi.jwtauthenticationbackend.service;
+
+import com.ravi.jwtauthenticationbackend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
